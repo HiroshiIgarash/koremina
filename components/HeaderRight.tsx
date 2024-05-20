3b0c5d4871e3aac5458ab21c57c9079b42293e93
igarashi-seez <igarashi@seez.jp>
2024-05-20 17:16:02 +0900

    ヘッダーポップオーバーが閉じるよう修正

--- components/HeaderRight.tsx
+++ components/HeaderRight.tsx
@@ -1,11 +1,7 @@
 import getCurrentUser from "@/app/action/getCurrentUser";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-import { Settings, BookmarkIcon } from "lucide-react";
-import Avatar from "./Avatar";
 import SignInButton from "./SignInButton";
 import SignOutButton from "./SignOutButton";
-import { Button } from "./ui/button";
-import Link from "next/link";
+import HeaderPopover from "./HeaderPopover";
 
 const HeaderRight = async () => {
   const currentUser = await getCurrentUser();
@@ -13,31 +9,9 @@ const HeaderRight = async () => {
   return (
     <div className="flex items-center gap-2">
       {currentUser ? (
-        <>
-          <Popover>
-            <PopoverTrigger>
-              <Avatar user={currentUser} />
-            </PopoverTrigger>
-            <PopoverContent className="w-fit flex flex-col">
-              <Button asChild variant="ghost" className="justify-start">
-                <Link href="/setting/">
-                  <Settings className="mr-2 w-4 h-4" />
-                  設定
-                </Link>
-              </Button>
-              <Button asChild variant="ghost" className="justify-start">
-                <Link href="/bookmark/">
-                  <BookmarkIcon className="mr-2 w-4 h-4" />
-                  ブックマーク
-                </Link>
-              </Button>
-              <SignOutButton className="w-full" variant="ghost" />
-            </PopoverContent>
-          </Popover>
-          <Button asChild>
-            <Link href="/post">投稿する</Link>
-          </Button>
-        </>
+        <HeaderPopover currentUser={currentUser}>
+          <SignOutButton className="w-full" variant="ghost" />
+        </HeaderPopover>
       ) : (
         <SignInButton />
       )}
