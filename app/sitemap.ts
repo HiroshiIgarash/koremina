24bf28c340a86d9aeb4c286d30a12b33c1583c96
igarashi-seez <igarashi@seez.jp>
2024-06-07 12:30:10 +0900

    投稿ページをsitemap.xmlに追加

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,36 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
+import getPosts from "./action/getPosts";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseURL = 'https://koremina.vercel.app'; // URLは.envに記載
+  const baseURL = "https://koremina.vercel.app"; // URLは.envに記載
   const lastModified = new Date();
+  const allPosts = await getPosts();
 
-  const staticPaths = [{
-    url: baseURL,
-    lastModified
-  }, {
-    url: `${baseURL}/about`,
-    lastModified
-  }, {
-    url: `${baseURL}/faq`,
-    lastModified
-  }, {
-    url: `${baseURL}/policy`,
-    lastModified
-  },
-];
+  const postPaths = allPosts.map((post) => {
+    return {
+      url: `${baseURL}/post/${post.id}`,
+      lastModified: post.postedAt,
+    };
+  });
 
-  return [ ...staticPaths ];
+  const staticPaths = [
+    {
+      url: baseURL,
+      lastModified,
+    },
+    {
+      url: `${baseURL}/about`,
+      lastModified,
+    },
+    {
+      url: `${baseURL}/faq`,
+      lastModified,
+    },
+    {
+      url: `${baseURL}/policy`,
+      lastModified,
+    },
+  ];
+
+  return [...staticPaths,...postPaths];
 }
