474152e316d0ef9f72dbb92213fa7a3cf1116f9f
HiroshiIgarashi <tubukiti2008@gmail.com>
2024-06-25 22:41:06 +0900

    PC 調整

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -10,41 +10,41 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <div className="px-4 w-full max-w-2xl mx-auto">
+    <div className="px-4 w-full max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-20 text-center">コレミナについて</h1>
-      <h2 className="text-5xl text-center leading-normal">
+      <h2 className="text-5xl md:text-6xl text-center leading-normal md:leading-relaxed">
         コレミナは
         <br />
-        <span className="text-[#2A4B71] font-bold">にじさんじ</span>の<br />
+        <span className="text-[#2A4B71] font-bold">にじさんじ</span>の<br className="md:hidden" />
         おすすめ動画を
         <br />
-        共有できる<br />サービスです！
+        共有できる<br className="md:hidden" />サービスです！
       </h2>
 
       <section className="mt-20">
         <h3 className="text-center text-3xl font-bold">こんなときに便利！</h3>
-        <div className="mt-12 space-y-8">
-          <div className="flex items-center gap-4">
-            <div className="p-4 border-4 border-green-400 rounded-2xl">
-              <p className="text-xl">
+        <div className="mt-12 space-y-8 max-w-2xl mx-auto">
+          <div className="flex justify-center items-center gap-4">
+            <div className="flex-1 p-4 border-4 border-green-400 rounded-2xl">
+              <p className="text-xl md:text-center">
                 推しについて
                 <span className="text-red-400">みんなに知ってほしい！</span>
               </p>
             </div>
             <Image className="shrink-0" src="/ota_boy.png" alt="" width={90} height={90} />
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex justify-center items-center gap-4">
             <Image className="shrink-0" src="/ota_girl.png" alt="" width={90} height={90} />
-            <div className="p-4 border-4 border-green-400 rounded-2xl">
-              <p className="text-xl">
+            <div className="flex-1 p-4 border-4 border-green-400 rounded-2xl">
+              <p className="text-xl md:text-center">
                 気になるライバーがいるから、
                 <span className="text-red-400">動画を見てみたい！</span>
               </p>
             </div>
           </div>
-          <div className="flex items-center gap-4">
-            <div className="p-4 border-4 border-green-400 rounded-2xl">
-              <p className="text-xl">
+          <div className="flex justify-center items-center gap-4">
+            <div className="flex-1 p-4 border-4 border-green-400 rounded-2xl">
+              <p className="text-xl md:text-center">
                 にじさんじを知ったばかり！
                 <span className="text-red-400">何から見よう...？</span>
               </p>
@@ -58,17 +58,17 @@ const Page = () => {
         <div className="mt-12 space-y-8">
           <div>
             <h4 className="text-center text-lg font-bold">登録編</h4>
-            <p className="mt-4">右上のログインボタンから登録しよう！GoogleアカウントかXアカウントがあればOK！<span className="text-red-400">登録はすぐ終わるよ！</span></p>
+            <p className="mt-4 text-center">右上のログインボタンから登録しよう！GoogleアカウントかXアカウントがあればOK！<span className="text-red-400">登録はすぐ終わるよ！</span></p>
           </div>
           <div>
   
           <h4 className="text-center text-lg font-bold">投稿編</h4>
-          <p className="mt-4">右上の投稿ボタンから投稿しよう！おすすめのYoutube動画のURLをコピーしてね！</p>
+          <p className="mt-4 text-center">右上の投稿ボタンから投稿しよう！おすすめのYoutube動画のURLをコピーしてね！</p>
           </div>
           <div>
   
           <h4 className="text-center text-lg font-bold">設定編</h4>
-          <p className="mt-4">設定ページから最推し、推しを設定できるよ！推しを設定すると動画を探しやすくなるよ！</p>
+          <p className="mt-4 text-center">設定ページから最推し、推しを設定できるよ！推しを設定すると動画を探しやすくなるよ！</p>
           </div>
         </div>
       </section>
