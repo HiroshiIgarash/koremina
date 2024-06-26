5a46517115c97ce1fba6a2586bd38b33402b0f95
HiroshiIgarashi <tubukiti2008@gmail.com>
2024-06-27 08:45:28 +0900

    KVを追加

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -3,16 +3,21 @@ import PostFilterContainer from "@/components/feature/post/PostFilterContainer";
 import PostList from "@/components/feature/post/PostList";
 import SkeletonPostList from "@/components/feature/post/SkeletonPostList";
 import { ChevronRight, Upload } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
+      <div className="mb-10 text-center">
+        <Image src="/kv_sp.png" className="md:hidden" width={800} height={420} alt="コレミナ -にじさんじおすすめ動画共有サービス-" />
+        <Image src="/kv_pc.png" className="hidden md:block w-[1000px] max-w-full" width={1280} height={420} alt="コレミナ -にじさんじおすすめ動画共有サービス-" />
+      </div>
     <div className="w-full px-4 mb-8 space-y-2 md:space-y-0 max-w-7xl mx-auto md:grid md:grid-cols-3 md:gap-4">
-      <div className="flex items-center w-full p-4 md:py-8 border-2 border-[#2A4B71] rounded-lg text-center gap-2">
-        <p className="text-lg">スマホの方は「ホーム画面に追加」でURLバーをなくせます</p>
-        <Upload className="shrink-0" size="1.4em" />
+      <div className=" items-center w-full p-4 md:py-8 bg-accent rounded-lg text-center gap-2">
+        <p className="text-lg">スマホの方は「ホーム画面に追加」<Upload className="shrink-0 inline" size="1em" />でURLバーをなくせます
+        </p>
       </div>
       <Link href="/about" className="relative flex items-center w-full p-4 md:py-8 border-2 border-[#2A4B71] rounded-lg text-center hover:bg-accent">
         <div className="flex-1">
