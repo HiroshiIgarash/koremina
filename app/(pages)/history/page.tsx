38e69035ae0c0eb33a532f5f0f45ea7888ad0d21
HiroshiIgarashi <tubukiti2008@gmail.com>
2024-06-28 22:25:34 +0900

    更新履歴を追加

--- app/(pages)/history/page.tsx
+++ app/(pages)/history/page.tsx
@@ -10,10 +10,10 @@ const Page = () => {
       <div className="px-4 w-full max-w-5xl mx-auto">
         <h1 className="text-3xl font-bold mb-16">更新履歴</h1>
         <div className="space-y-4">
-          {/* <div>
-            <time dateTime="2024-06-13" className="font-bold">2024/06/12</time><br /> 
-            <p>Google Search Console に登録しました。</p>
-          </div> */}
+          <div>
+            <time dateTime="2024-06-29" className="font-bold">2024/06/12</time><br /> 
+            <p>コレミナ -にじさんじおすすめ動画共有サービス（非公式）- を正式にリリースしました！</p>
+          </div>
           <div>
             <time dateTime="2024-06-12" className="font-bold">2024/06/12</time><br /> 
             <p>Google Search Console に登録しました！</p>
