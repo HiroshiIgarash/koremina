f3143027eae5ca9a0e4b218e346e45f8e9cb0013
igarashi-seez <igarashi@seez.jp>
2024-07-02 19:03:52 +0900

    投稿者でなくても編集ページに入れてしまう不具合を修正

--- app/(pages)/post/edit/[postId]/page.tsx
+++ app/(pages)/post/edit/[postId]/page.tsx
@@ -1,11 +1,21 @@
+import getPostById from "@/app/action/getPostById";
+import { auth } from "@/auth";
 import PostEditContainer from "@/components/feature/post/PostEditContainer";
+import { redirect } from "next/navigation";
 
 interface IParam {
   postId: string;
 }
 
-const Page = ({params}:{params:IParam}) => {
-  const {postId} = params
+const Page = async ({ params }: { params: IParam }) => {
+  const session = await auth();
+  const { postId } = params;
+  const post = await getPostById(postId);
+
+  if (!post || !session || post.postedUserId !== session.user?.id) {
+    redirect("/");
+  }
+
   return (
     <div className="w-full max-w-2xl mx-auto px-4">
       <PostEditContainer postId={postId} />
