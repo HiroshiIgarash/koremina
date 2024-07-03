a857a484e203b39327822f19898ae3b5292a77bb
igarashi-seez <igarashi@seez.jp>
2024-07-03 19:04:15 +0900

    Dockerを導入

---
+++ app/(pages)/check/page.tsx
@@ -0,0 +1,22 @@
+import prisma from "@/lib/db"
+import { Video } from "@prisma/client"
+
+const Page = async () => {
+
+  const posts = await prisma.video.findMany({
+    where: {
+      detailComment: {
+        not: ''
+      }
+    }
+  })
+  return (
+    <div>
+      {posts.toSorted((b,a) => (a.detailComment!.length - b.detailComment!.length) ).map((post,index) =>(
+        <p key={post.id} className="flex"><span>{index+1}/{posts.length}</span> ({post.detailComment!.length}){post.detailComment}</p>
+        ))}
+    </div>
+  )
+}
+
+export default Page
