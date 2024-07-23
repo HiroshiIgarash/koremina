6831446231d0ddbff69a31ad869db215596197c6
HiroshiIgarashi <tubukiti2008@gmail.com>
2024-07-23 09:27:23 +0900

    更新履歴の追記

--- app/(pages)/history/page.tsx
+++ app/(pages)/history/page.tsx
@@ -11,6 +11,16 @@ const Page = () => {
         <h1 className="text-3xl font-bold mb-16">更新履歴</h1>
         <div className="space-y-8">
           <div>
+            <time dateTime="2024-07-23" className="font-bold">
+              2024/07/23
+            </time>
+            <ul className="my-2 ml-6 list-disc [&>li]:mt-2">
+              <li>
+                GitHubへのリンクを追加しました。
+              </li>
+            </ul>
+          </div>
+          <div>
             <time dateTime="2024-07-06" className="font-bold">
               2024/07/06
             </time>
