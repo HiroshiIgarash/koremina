c408aeefe128d5da78acb929a852d8209e39b86a
HiroshiIgarash <tubukiti2008@gmail.com>
2025-12-28 09:07:47 +0900

    chore: 鈴原るるの卒業ステータスをfalseに変更

--- app/(pages)/history/page.tsx
+++ app/(pages)/history/page.tsx
@@ -11,6 +11,16 @@ const Page = () => {
         <h1 className="text-3xl font-bold mb-16">更新履歴</h1>
         <div className="space-y-8">
           <div>
+            <time dateTime="2025-10-28" className="font-bold">
+              2025/12/28
+            </time>
+            <ul className="my-2 ml-6 list-disc [&>li]:mt-2">
+              <li>
+                「鈴原るる」を卒業ライバーからにじさんじ所属ライバーに変更しました。
+              </li>
+            </ul>
+          </div>
+          <div>
             <time dateTime="2025-10-04" className="font-bold">
               2025/10/04
             </time>
